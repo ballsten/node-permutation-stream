22e0dca588997c34d531c977423c65e1c3759fce
Andrew Theaker <andrew@theaker.name>
2013-10-17 16:17:20 +1100

    Add test stub

---
+++ test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+var es = require('event-streams');
+var concat = require('concat');
+
+describe('permutation streams', function() {
+  it('should return all permutations');
+});
