22382c2f98a9dcd8f428ef50b47c68d628e32f7e
Andrew Theaker <andrew@theaker.name>
2013-10-17 17:47:23 +1100

    Implement first test

--- test.js
+++ test.js
@@ -1,7 +1,15 @@
 var expect = require('chai').expect;
-var es = require('event-streams');
+var es = require('event-stream');
 var concat = require('concat');
 
+var ps = require('./index');
+
 describe('permutation streams', function() {
-  it('should return all permutations');
+  it('should return all permutations', function(done) {
+    var s = ps.createPermutationStream();
+    es.readArray(["hello", "world"]).pipe(s).pipe(concat(function(data) {
+      var expected = [ "hello", "world", "helloworld", "worldhello"];
+      expect(data.sort).to.eql(expected.sort());
+    }));
+  });
 });
