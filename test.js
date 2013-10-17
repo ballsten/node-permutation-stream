af159fb18a817902458e02857f4723520a7d19de
Andrew Theaker <andrew@theaker.name>
2013-10-18 08:46:24 +1100

    Remove dependency on concat (replaced with es.writeArray)

--- test.js
+++ test.js
@@ -1,13 +1,12 @@
 var expect = require('chai').expect;
 var es = require('event-stream');
-var concat = require('concat');
 
 var ps = require('./index');
 
 describe('permutation streams', function() {
   it('should return all permutations', function(done) {
     var s = ps.createPermutationStream();
-    es.readArray(["hello", "world"]).pipe(s).pipe(concat(function(data) {
+    es.readArray(["hello", "world"]).pipe(s).pipe(es.writeArray(function(err, data) {
       var expected = [ "hello", "world", "helloworld", "worldhello"];
       expect(data.sort).to.eql(expected.sort());
     }));
