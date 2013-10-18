8f9a36e1d2427c5ebba91b27ce60971d734f547f
Andrew Theaker <andrew@theaker.name>
2013-10-18 14:42:35 +1100

    Add test with 3 words. Passes

--- test.js
+++ test.js
@@ -4,10 +4,27 @@ var es = require('event-stream');
 var ps = require('./index');
 
 describe('permutation streams', function() {
-  it('should return all permutations', function(done) {
+  it('should return all permutations for two words', function(done) {
     var s = ps.createPermutationStream();
     es.readArray(["hello", "world"]).pipe(s).pipe(es.writeArray(function(err, data) {
       var expected = [ [], ["hello"], ["world"], ["hello", "world"], ["world", "hello"] ];
+      expect(data.sort()).to.eql(expected.sort());
+      done();
+    }));
+  });
+
+  it('should return all permutations for 3 words', function(done) {
+    var s = ps.createPermutationStream();
+    es.readArray(["hello", "world", "one" ]).pipe(s).pipe(es.writeArray(function(err, data) {
+      var expected = [ [], 
+        ["hello"], ["world"], ["one"],
+        ["hello", "world"], ["hello", "one"],
+        ["world", "hello"], ["world", "one"],
+        ["one", "world"], ["one", "hello"],
+        ["hello", "world", "one"], ["hello", "one", "world"], 
+        ["world", "hello", "one"], ["world", "one", "hello"], 
+        ["one", "world", "hello"], ["one", "hello", "world"]
+      ];
       expect(data.sort()).to.eql(expected.sort());
       done();
     }));
